ec733aadc10e044875d899cfac451cf117511ba1
DsunilK <sunilkumardevalla@gmail.com>
2021-05-17 19:27:01 -0500

    Add model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :comments
   validates :username, presence: true
   validates :password, presence: true
